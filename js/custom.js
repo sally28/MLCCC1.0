4638f1186192a2de3b2ca59c1a3bde4aa9c4a472
Sally <sallychen426@gmail.com>
2020-04-21 18:00:11 -0400

    modify newsletter page

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,16 @@
 jQuery(document).ready(function() {
 	
 	"use strict";
-    $("#includedContent").load("newsContent.html");
-    $("#includedContent").load("newsContent915.html");
-
-     $('.showSingle').click(function(){
-        $('.targetDiv').hide();
+    //$("#includedContent").load("newsContent.html");
+    //$("#includedContent").load("newsContent915.html");
+    //$("#includedContent").load("newsContent908.html");
+     $('.newsingle').click(function(){
+         var targetId = this.getAttribute('target');
+         var fileName = "newsletter/newsContent"+targetId+".html";
+         $("#includedContent").load(fileName);
+        /*$('.targetDiv').hide();
         $('#includedContent'+jQuery(this).attr('target')).show();
-        $(this).addClass('active').siblings().removeClass('active');
+        $(this).addClass('active').siblings().removeClass('active');*/
     });
 
       $('.showSingle').click(function(){
@@ -45,5 +48,6 @@ jQuery(document).ready(function() {
         }
     })*/
 });
+
 
 
