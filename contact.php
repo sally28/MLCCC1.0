5e727cbd7ac43d10848ff4cdc9c125cfbd778dcf
Sally <sallychen426@gmail.com>
2020-07-06 17:08:15 -0400

    finalize contact form

--- contact.php
+++ contact.php
@@ -1,13 +1,54 @@
 <?php
-  if($_POST['submit']){
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+//use PHPMailer\PHPMailer;
+//use phpmailer\SMTP;
+//use phpmailer\Exception;
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
 
-  $result='Form submitted';
+/* Load Composer's autoloader
+require 'vendor/autoload.php';*/
 
-  }
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+
+try {
+    //Server settings
+   // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'mlccccamp@gmail.com';                     // SMTP username
+    $mail->Password   = 'zhongwen160';                               // SMTP password
+    $mail->SMTPSecure = PHPMailer\PHPMailer\PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    //Recipients
+    $mail->setFrom($_POST['email'], $_POST['name']);
+    // $mail->addAddress('info@gmail.com', 'MLCCC Admin');     // Add a recipient
+    $mail->addAddress('mlcccwndschool@gmail.com');               // Name is optional
+    $mail->addReplyTo($_POST['email']);
+    //$mail->addReplyTo($_POST['inputEmail'], $_POST['inputName'])；
+     //$mail->addCC('cc@example.com');
+    //$mail->addBCC('bcc@example.com');
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = $_POST['subject'];
+    $mail->Body    = $_POST['message'];
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+
+
 
 ?>
-
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -57,7 +98,7 @@
               <li><a href="support.html">Support Us</a></li>
               <li>
                  <a href="https://mlcccadmin.herokuapp.com" target="_blank" >Login</a>
-              </li> 
+              </li>
             </ul>
           </div>
         </div>
@@ -71,7 +112,7 @@
           </div>
         </div>
       </section>-->
-      
+
 
       <section class="probootstrap-section probootstrap-section-sm">
         <div class="container">
@@ -110,7 +151,7 @@
           </div>
 
       </section>
-      
+
       <section class="probootstrap-cta">
         <div class="container">
           <div class="row">
